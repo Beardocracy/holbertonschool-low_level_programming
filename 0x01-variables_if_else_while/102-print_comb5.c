057ad70f4b6e7160aba8f30b0dc1183a46092659
Beardocracy <travisjbearden@gmail.com>
2019-09-20 22:25:45 +0000

    Redid my entire strategy

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+  * main - Prints all possible combos of two two-digit numbers
+  * Return:0
+  */
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (b < 99)
+	{
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+		putchar(' ');
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+		if (b < 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+		if ((a % 100) == 0)
+		{
+			b++;
+			a = b + 1;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
