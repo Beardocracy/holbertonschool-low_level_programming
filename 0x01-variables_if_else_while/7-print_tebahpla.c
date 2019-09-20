f9b6f0d524f62c9e94b9ab7acbe629d8a5927340
Beardocracy <travisjbearden@gmail.com>
2019-09-20 01:46:56 +0000

    Added part 7 for testing

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - prints all single digit numbers of base 10 starting at 0
+  * Return:0
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 122; a > 96; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
