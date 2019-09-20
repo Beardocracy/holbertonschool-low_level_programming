5fe0aa25552fa6d0253a61166160a1ea6c179e6b
Beardocracy <travisjbearden@gmail.com>
2019-09-20 20:18:56 +0000

    Changed from perror to write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 /**
   * main - prints a string to the standard error
   * Return: 1
@@ -7,7 +7,8 @@
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
