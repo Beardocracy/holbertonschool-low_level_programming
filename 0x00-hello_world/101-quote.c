410d982693257f0bdb165b59a08d58409a77a3b0
Beardocracy <travisjbearden@gmail.com>
2019-09-20 18:14:30 +0000

    Added part 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <errorno.h>
+/**
+  * main - prints a string to the standard error
+  * Return: 1
+  */
+
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1)
+}
