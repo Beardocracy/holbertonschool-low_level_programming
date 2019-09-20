5da14a5221e7d7cf69002387e633863d41c53dd0
Beardocracy <travisjbearden@gmail.com>
2019-09-20 19:42:38 +0000

    Added correct library for perror

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 /**
   * main - prints a string to the standard error
   * Return: 1
@@ -6,6 +7,7 @@
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	
 	return (1);
 }
