688c2376af08150abe2235bd4d5fc4a9470c61ba
Beardocracy <travisjbearden@gmail.com>
2019-09-23 23:06:08 +0000

    Added part 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * main - prints Holberton, followed by a new line
+  * Return: 0
+  */
+
+int main(void)
+{
+	char name[9] = "Holberton";
+	int a = 0;
+
+	for (a = 0; a < 9; a++)
+		_putchar(name[a]);
+
+	return (0);
+}
