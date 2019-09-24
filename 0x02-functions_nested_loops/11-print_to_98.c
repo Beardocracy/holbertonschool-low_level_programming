3b06703bf0b8bd8b132069abc2aef7d3986db2d9
Beardocracy <travisjbearden@gmail.com>
2019-09-24 21:18:25 +0000

    Added part 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * print_to_98 - prints all natural numbers n to 98, followed by newline
+  * @n: integer input
+  * Return: void
+  */
+
+void print_to_98(int n)
+{
+	while (n > 98 || n < 98)
+	{
+		if ( n < 0)
+			_putchar('-');
+		if ((abs(n)) < 10)
+			_putchar((abs(n)) + '0');
+		else if ((abs(n)) < 100)
+		{
+			_putchar(((abs(n)) / 10) + '0');
+			_putchar(((abs(n)) % 10) + '0');
+		}
+		else
+		{
+			_putchar(((abs(n)) / 100) + '0');
+			_putchar((((abs(n)) / 10) % 10) + '0');
+			_putchar(((abs(n)) % 10) + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
