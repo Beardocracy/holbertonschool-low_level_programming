e69c8c95da2eedef646bf74c34ce9d76e68d82ac
Beardocracy <travisjbearden@gmail.com>
2019-09-24 15:59:11 +0000

    Added holberton.h file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,10 @@
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+
+#endif
