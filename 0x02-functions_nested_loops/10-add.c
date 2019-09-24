5a1a44e28f116f107f5fff6976dd08375d626ec1
Beardocracy <travisjbearden@gmail.com>
2019-09-24 20:33:44 +0000

    Added part 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * add - adds 2 integers
+  * @a: integer 1
+  * @b: integer 2
+  * Return: result of addition
+  */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
