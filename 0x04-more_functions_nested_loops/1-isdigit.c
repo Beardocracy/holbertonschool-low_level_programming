60336f62148c29a03d9176847fd19e0b74fbf3a9
Beardocracy <travisjbearden@gmail.com>
2019-09-26 23:43:52 +0000

    Added part 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * _isdigit - checks for a digit
+  * @c: integer
+  * Return: 1 if c is a digit, 0 otherwise
+  */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
