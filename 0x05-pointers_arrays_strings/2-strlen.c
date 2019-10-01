71278250fd86eaab8e5ec645b775f94ac523597c
Beardocracy <travisjbearden@gmail.com>
2019-10-01 16:00:38 +0000

    Added part 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * _strlen - returns the length of a string
+  * @s: pointer to a string
+  * Return: length of string (int)
+  */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+
+	return (i);
+}
