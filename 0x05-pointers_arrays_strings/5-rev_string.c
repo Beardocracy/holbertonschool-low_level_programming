69e540b17a06f6db5a994e6e4fe7caf3c22e4a44
Beardocracy <travisjbearden@gmail.com>
2019-10-01 17:29:55 +0000

    Added part 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+  * rev_string - reverses a string
+  * @s: string
+  */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char tmp[100];
+
+	while (s[i] != '\0')
+	{
+		tmp[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[j] = tmp[i];
+		i--;
+		j++;
+	}
+	s[j] = '\0';
+}
