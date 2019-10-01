1641c40765f7b4f06a984ba2298dbdb61c862911
Beardocracy <travisjbearden@gmail.com>
2019-10-01 19:53:44 +0000

    Corrected len

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,17 @@
 void puts_half(char *str)
 {
 	int len = 0;
+	int n;
 
 	while (str[len] != '\0')
 		len++;
+	n = len / 2;
 	if (len % 2 == 1)
-		len = (len - 1) / 2;
-	else
-		len /= 2;
-	while (str[len] != '\0')
+		n++;
+	while (str[n] != '\0')
 	{
-		_putchar(str[len]);
-		len++;
+		_putchar(str[n]);
+		n++;
 	}
 	_putchar('\n');
 }
