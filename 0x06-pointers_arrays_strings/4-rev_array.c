6c1607ca722c162d9217888817fbc14f5dfc1f88
Beardocracy <travisjbearden@gmail.com>
2019-10-03 20:32:00 +0000

    Added part 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+  * reverse_array - reverses the content of an array of integers
+  * @a: pointer to integer array
+  * @n: the number of integers in the array to be reversed
+  */
+
+void reverse_array(int *a, int n)
+{
+	int t, i;
+
+	n = n - 1;
+	i = 0;
+	do {
+		t = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = t;
+		i++;
+		n--;
+	} while (i <= n);
+}
