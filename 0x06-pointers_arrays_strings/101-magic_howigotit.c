6162edc937b60308d5f4c20260537d248350482d
Beardocracy <travisjbearden@gmail.com>
2019-10-05 01:54:55 +0000

    Added part 101 with reference for how i solved it

--- 0x06-pointers_arrays_strings/101-magic_howigotit.c
+++ 0x06-pointers_arrays_strings/101-magic_howigotit.c
@@ -16,7 +16,17 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+  *(p + 4) = 98;
+  	printf("Address of n   = %p\n", &n);
+	printf("Address of p   = %p\n", p);
+	printf("Address of &p  = %p\n", &p);
+	printf("Address of a   = %p\n", a);
+	printf("Adress of  &a  = %p\n", &a);
+	printf("*(p + 5) = %d\n", *(p + 5));
+	printf("*(p + 6) = %d\n", *(p + 6));
+	printf("*(p + 7) = %d\n", *(p + 7));
+	printf("*(p + 8) = %d\n", *(p + 8));
+			
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
