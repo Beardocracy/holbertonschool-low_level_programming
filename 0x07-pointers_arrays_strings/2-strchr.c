6cbee99d9759a5245a3d3ea6e85aa9af4de7e961
Beardocracy <travisjbearden@gmail.com>
2019-10-07 19:22:30 +0000

    Added part 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * _strchr - returns a pointer to the first occurrence of char c in s.
+  * @s: pointer to a string
+  * @c: char to be located
+  * Return: pointer pointed to the first occurrence of c.
+  */
+
+char *_strchr(char *s, char c)
+{
+	for ( ; *s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (s);
+}
