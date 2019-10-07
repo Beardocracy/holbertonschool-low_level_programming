8f79a0f37a6e398498efab5e204ea4c9403ff0e7
Beardocracy <travisjbearden@gmail.com>
2019-10-07 20:46:08 +0000

    Added part 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+  * _strpbrk - searches a string for any set of bytes
+  * @s: string to be searched
+  * @accept: set of characters to search for
+  * Return: pointer to first matched character in s
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (j = 0; accept[j] != 0; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (NULL);
+}
