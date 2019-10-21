e6973c48f2fe3a7326b441441f5a7d1d146f89a2
Beardocracy <travisjbearden@gmail.com>
2019-10-21 18:11:32 +0000

    Initialized counter variable outside of loop so it would work in special case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,8 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	int len1, len2, i, j, count;
 
+	count = 0;
+
 	for (len1 = 0; s1[len1]; len1++)
 		;
 	for (len2 = 0; s2[len2]; len2++)
@@ -19,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	cat = malloc(sizeof(*cat) * (len1 + len2) + 1);
 	if (cat == NULL)
 		return (NULL);
-	for (i = 0, count = 0; s1[i]; i++, count++)
+	for (i = 0; s1[i]; i++, count++)
 		cat[count] = s1[i];
 	for (j = 0; s2[j]; j++, count++)
 		cat[count] = s2[j];
