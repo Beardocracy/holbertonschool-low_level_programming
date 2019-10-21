b45ece0bbb8f11071757cf533e3f4ca3ab9311fd
Beardocracy <travisjbearden@gmail.com>
2019-10-21 21:22:16 +0000

    Added part 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - structure contains relevant doggy info
+  * @name: char pointer to the dogs name
+  * @age: float age of dog
+  * @owner: char pointer to name of owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+ #endif /* DOG_H */
