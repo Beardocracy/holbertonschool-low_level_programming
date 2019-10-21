a09efe347c5efd728286d71705da4bae61a34f56
Beardocracy <travisjbearden@gmail.com>
2019-10-21 17:06:00 +0000

    Ditched the null character as is not needed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,8 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = malloc(sizeof(*arr) * size + 1);
+	arr = malloc(sizeof(*arr) * size);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	arr[size] = '\0';
 	return (arr);
 }
