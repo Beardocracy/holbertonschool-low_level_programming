8cc2343aa9cc879ccba3bf18ef76f6ea90b04395
Beardocracy <travisjbearden@gmail.com>
2019-10-21 16:49:18 +0000

    Changed comparison of size > 0 to size == 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
-	if (size < 1)
+	if (size == 0)
 		return (NULL);
 	arr = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
