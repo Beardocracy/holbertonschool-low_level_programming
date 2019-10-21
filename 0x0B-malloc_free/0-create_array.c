e709273cddcc21cb8f91b84fceccf301cd2c304d
Beardocracy <travisjbearden@gmail.com>
2019-10-21 17:04:07 +0000

    Added one extra byte in malloc to account for null char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = malloc(sizeof(char) * size);
+	arr = malloc(sizeof(*arr) * size + 1);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 	arr[size] = '\0';
