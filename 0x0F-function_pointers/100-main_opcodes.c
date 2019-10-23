74101b2752448374eb319ae3179387b3d15cc772
Beardocracy <travisjbearden@gmail.com>
2019-10-23 22:53:40 +0000

    Added part 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - prints the opcodes of its own main function.
+  * @argc: the number of elements
+  * @argv: an array of strings passed in to the function.
+  * Return: 1 if number of arguments is incorrect, 2 if number of bytes is
+  * negative.
+  */
+int main(int argc, char *argv[])
+{
+	printf("Opcodes yo");
+	exit(0);
+}
