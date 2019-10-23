6ee054db6970216fbf4fbec18581a38ffdb30eb5
Beardocracy <travisjbearden@gmail.com>
2019-10-23 19:22:23 +0000

    Added case for nulls in part 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
   * print_name - receives a function ptr and string. Sends string to function.
@@ -7,5 +8,7 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL)
+		return;
 	f(name);
 }
