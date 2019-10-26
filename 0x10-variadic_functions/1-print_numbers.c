fc74fd4dec8cec09e0575218884908e03a1369a9
Beardocracy <travisjbearden@gmail.com>
2019-10-26 19:42:29 +0000

    Removed spaces after va_ functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	va_start (ap, n);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
@@ -24,5 +24,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i == n - 1)
 			printf("\n");
 	}
-	va_end (ap);
+	va_end(ap);
 }
