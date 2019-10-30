efa8c057abc7f322e7710e1c22edb6b17a375b71
Beardocracy <travisjbearden@gmail.com>
2019-10-30 21:15:38 +0000

    Added part 100 for testing

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,26 +1,17 @@
-	.file	"100-hello_holberton.c"
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+section .data
+
+    message db "Hello, Holberton", 10
+
+section .text
+
+global main
 main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	call	puts
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 5.5.0-12ubuntu1~14.04) 5.5.0 20171010"
-	.section	.note.GNU-stack,"",@progbits
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
