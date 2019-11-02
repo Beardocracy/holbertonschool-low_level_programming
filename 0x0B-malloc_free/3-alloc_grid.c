5594270db396bea2550507fb80a3421386c05884
Beardocracy <travisjbearden@gmail.com>
2019-11-02 23:30:27 +0000

    Added for case of width or height is zero

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,10 +13,11 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **grid;
 
+	if (width == 0 || height == 0)
+		return (NULL);
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
-
 	for (i = 0; i <= width; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
