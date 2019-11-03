b9a9cc40fc2a7931dd5263403571753934b95e91
Beardocracy <travisjbearden@gmail.com>
2019-11-03 00:29:51 +0000

    Added if statement for null arguments passed.

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -11,6 +11,8 @@ char *argstostr(int ac, char **av)
 	int i, j, count = 0, z = 0;
 	char *s;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
