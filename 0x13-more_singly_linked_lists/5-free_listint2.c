764add546e2ec040e9e3127b59c1b51d8729bbb7
Beardocracy <travisjbearden@gmail.com>
2019-11-05 23:51:53 +0000

    Changed back

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,12 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *node;
-	node = *head;
 
-	while (node)
+	while (*head)
 	{
-		temp = node->next;
-		free(node);
-		node = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	head = NULL;
 }
