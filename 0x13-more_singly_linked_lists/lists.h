3d648a2795339093737c479732db7c7c294d46e5
Beardocracy <travisjbearden@gmail.com>
2019-11-06 08:03:12 +0000

    Cleaned up for betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,17 +19,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct listint_s - singly linked list
- * @n: unsigned long integer
- * @next: points to the next node
- */
-typedef struct list_lu_int_s
-{
-	unsigned long int n;
-	struct list_lu_int_s *next;
-} listint_u;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
