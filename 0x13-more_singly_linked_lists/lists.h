8fc37cafaee4ac4de0636b87f6e2a4ff8be6d4a6
Beardocracy <travisjbearden@gmail.com>
2019-11-06 08:09:39 +0000

    Removed unnecessary prototypes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,7 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-listint_u *repeat_number_checker(listint_u **head, unsigned long int n);
 size_t free_listint_safe(listint_t **h);
 void loop_cutter(listint_t *loop);
 listint_t *loop_start(listint_t *p, listint_t *head);
