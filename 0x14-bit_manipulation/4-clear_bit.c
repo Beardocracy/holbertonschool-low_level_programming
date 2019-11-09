f725f61b85d9895679069282b1a232924ba7adc6
Beardocracy <travisjbearden@gmail.com>
2019-11-09 01:05:29 +0000

    Added part 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - Sets the value of a bit to 1 at a given index.
+ * @n: pointer to the ul int to change
+ * @index: the bit to set to 1.
+ * Return: 1 if successful, -1 if index exceeds ulint max bits.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int one = 1;
+	size_t unsigned_long_int_index_max;
+
+	unsigned_long_int_index_max = (sizeof(unsigned long int) * 8);
+	if (index >= unsigned_long_int_index_max)
+		return (-1);
+
+	if (((*n >> index) & 1) == 1)
+		*n -= (one << index);
+	return (1);
+}
