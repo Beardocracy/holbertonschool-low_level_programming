49887fa98bff1b1102b4d43d12b35ff1900cdb2a
Beardocracy <travisjbearden@gmail.com>
2019-11-10 21:27:06 +0000

    Added part 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: size of memory to allocate.
+ * Return: a void pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdint.h>
+
+int _putchar(char);
+void *malloc_checked(unsigned int b);
+
+#endif
