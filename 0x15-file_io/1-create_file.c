189c0b17b9ddd8bbe382d2bcbc264b94dc5c0849
Beardocracy <travisjbearden@gmail.com>
2019-11-13 03:53:23 +0000

    Added part 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create.
+ * @text_content: string to write to the file.
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	write(fd, text_content, i);
+
+	close(fd);
+
+
+	return (1);
+}
