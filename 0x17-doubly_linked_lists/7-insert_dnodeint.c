80fddb9f211d82610e7039189835031d9520cabb
Beardocracy <travisjbearden@gmail.com>
2019-12-15 21:37:03 +0000

    Added statement to change the next nodes prev value

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,6 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->next = temp->next;
 			new->prev = temp;
 			temp->next = new;
+			(new->next)->prev = new;
 			return (new);
 		}
 	}
