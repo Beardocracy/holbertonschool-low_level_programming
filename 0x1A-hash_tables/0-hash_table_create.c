3414649c2c08c3ad1e51a3d84d4bdfc2719801fd
Beardocracy <travisjbearden@gmail.com>
2020-01-23 20:09:49 +0000

    Changed structure of table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * hash_table_create - creates a hash table
@@ -9,14 +10,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
+	hash_node_t *node_ptrs;
 
 	if (size == 0)
 		return (NULL);
 
-	new = malloc(sizeof(hash_table_t *) * size);
+	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
+	new->size = size;
+
+	node_ptrs = malloc(sizeof(hash_node_t *) * size);
+	if (node_ptrs == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->array = &node_ptrs;
 
 	return (new);
 }
-
