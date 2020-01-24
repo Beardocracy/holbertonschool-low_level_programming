a1d49ad2dab9d42a7d1c289efb29678c8dfa0267
Beardocracy <travisjbearden@gmail.com>
2020-01-24 00:13:29 +0000

    Removed extra variable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *temp;
+	hash_node_t *new_node;
 	unsigned long int hi_key;
 
 	if (ht == NULL || key == NULL)
