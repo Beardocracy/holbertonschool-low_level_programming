3a50c41e0dd86cac3227b4f01f8ae483c701263b
Beardocracy <travisjbearden@gmail.com>
2020-04-15 21:58:47 +0000

    Added part 9

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * interpolation_search - searches for a value in a sorted array
+ * @array: a pointer to the first position in the array
+ * @size: size of the array
+ * @value: the target value
+ * Return: index where the value is found, or -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low, high;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		pos = low + (((double)(high - low) /
+			(array[high] - array[low])) * (value - array[low]));
+		if (pos >= size)
+		{
+			printf("Value check array[%lu] ", pos);
+			printf("is out of range\n");
+			return (-1);
+		}
+		printf("Value checked array[%lu] = %d\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] < value)
+			low = pos;
+		else
+			high = pos;
+
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_recursive(int *array, size_t l, size_t r, int value);
 int jump_search(int *array, size_t size, int value);
+size_t mmin(size_t a, size_t b);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
