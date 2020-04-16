267a2d02348e43885c2bf0b9c5ab7c3dc60c19ac
Beardocracy <travisjbearden@gmail.com>
2020-04-16 22:02:06 +0000

    Added part 104

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,57 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - finds the index of a value in an array
+ * @array: sorted integer array
+ * @size: the size of the array
+ * @value: the target
+ * Return: the first index of the value, or -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (adv_bin_recursive(array, 0, size - 1, value));
+}
+
+/**
+ * adv_bin_recursive - searches for a value in a sorted array with BS
+ * @array: pointer to the first element of the array
+ * @l: the leftmost index
+ * @r: the rightmost index
+ * @value: The value to search for
+ * Return: index where value is found, or -1
+ */
+int adv_bin_recursive(int *array, size_t l, size_t r, int value)
+{
+	size_t mid;
+	size_t l_temp = l;
+
+	if (r >= l)
+	{
+		mid = l + (r - l) / 2;
+		printf("Mid: %lu, array[%lu] = %d", mid, mid, array[mid]);
+		printf(" | Value = %d\n",  value);
+		printf("Searching in array: ");
+		while (l_temp <= r)
+		{
+			printf("%d", array[l_temp]);
+			l_temp++;
+			if (l_temp <= r)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		if (array[mid] == value)
+		{
+			if (mid != 0 && array[mid - 1] == value)
+				return (adv_bin_recursive(array, l, mid, value));
+			return (mid);
+		}
+		if (array[mid] > value)
+			return (adv_bin_recursive(array, l, mid - 1, value));
+		return (adv_bin_recursive(array, mid + 1, r, value));
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,7 @@ int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 size_t mmin(size_t s, size_t n);
+int advanced_binary(int *array, size_t size, int value);
+int adv_bin_recursive(int *array, size_t l, size_t r, int value);
 
 #endif
